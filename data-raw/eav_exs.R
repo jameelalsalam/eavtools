26786754d8ed5ee0cf0100854ec4a4ce3d6de2e5
Jameel Alsalam <alsalam.jameel@epa.gov>
2018-10-23 09:33:26 -0400

    add small emissions dataset

--- data-raw/eav_exs.R
+++ data-raw/eav_exs.R
@@ -2,10 +2,11 @@
 
 library(tidyverse)
 
-tiny <- readr::read_csv("data-raw/tiny.csv") %>%
+eav_tiny <- readr::read_csv("data-raw/tiny.csv") %>%
 	rlang::set_attrs("spec" = NULL)
-usethis::use_data(tiny, overwrite = TRUE)
+usethis::use_data(eav_tiny, overwrite = TRUE)
 
-tinyeav <- tiny %>%
-	eavtools::new_tbl_eav("empid", "var", "value")
-usethis::use_data(tinyeav, overwrite = TRUE)
+eav_emissions <- readr::read_csv("data-raw/emissions.csv") %>%
+	rlang::set_attrs("spec" = NULL)
+usethis::use_data(eav_emissions, overwrite = TRUE)
+
